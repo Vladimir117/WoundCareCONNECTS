794d91f2001969f16ad3a3f4cfc9485d99e49f42
mostspace <mostspace1027@gmail.com>
2024-07-12 08:16:37 -0700

    structure

---
+++ server/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const session = require('express-session');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+
+
+const path = require('path');
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+});
+
+// Serve static files from the 'uploads' directory
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.use(express.json());
+app.use(cors());
+app.use(session({ secret: 'your-secret-key', resave: false, saveUninitialized: false }));
+
+connectDB();
+
+app.use('/api/users', userRoutes);
+
+app.listen(3003, () => {
+  console.log("Server is running on port 3003");
+});
