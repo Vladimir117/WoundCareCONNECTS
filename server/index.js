51e303b30c6f71c68d90312b5ba7cc2b92aa9eec
mostspace <mostspace1027@gmail.com>
2024-07-17 05:01:10 -0700

    npm

--- server/index.js
+++ server/index.js
@@ -5,11 +5,12 @@ const session = require('express-session');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 
+
 const path = require('path');
 const app = express();
 
 // Serve static files from the 'uploads' directory
-// app.use('/uploads', express.static(path.join(__dirname, '../client/public/uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(express.json());
 app.use(cors());
